9384e207896ddcc09b1b48e8488e908687c437cf
itxmal03 <itxmal03@users.noreply.github.com>
2026-01-13 12:17:44 -0700

    set of 3 elements sum done by 2 methods

---
+++ Day_04/file.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+void sum3Elements(int array[10], int size);
+void sum3SlidigElements(int array[10], int size);
+
+int main()
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    // sum3Elements(arr, size);
+    sum3SlidigElements(arr, size);
+
+    return 0;
+}
+
+void sum3Elements(int array[10], int size)
+{
+    // Method #01
+    // int sum = 0, counter = 0, setNo = 1;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     sum = sum + array[i];
+    //     counter++;
+    //     if (counter == 3)
+    //     {
+    //         cout << "Sum of " << setNo << " set of 3 numbers is " << sum << endl;
+    //         sum = 0;
+    //         counter = 0;
+    //         setNo++;
+    //     }
+    // }
+
+    int sum = 0, setNo = 1;
+    // Method 2
+    for (int i = 0; i + 2 < size; i = i + 3)
+    {
+        sum = array[i] + array[i + 1] + array[i + 2];
+        cout << "Sum of " << setNo << " set of 3 numbers is " << sum << endl;
+        setNo++;
+    }
+}
+
+void sum3SlidigElements(int array[10], int size)
+{
+    // Method #01
+    // int sum = 0, setNo = 1;
+    // for (int i = 0; i + 2 < size; i++)
+    // {
+    //     sum = array[i] + array[i + 1] + array[i + 2];
+    //     cout << "Sum of " << setNo << " set of 3 numbers is " << sum << endl;
+    //     setNo++;
+    // }
+   
+    //Method 2 
+    
+}
