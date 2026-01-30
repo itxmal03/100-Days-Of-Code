5cac9571a4623a8548159f805c0cdde15ed14335
itxmal03 <itxmal03@users.noreply.github.com>
2026-01-30 22:19:14 +0500

    missing number from array & add 1 to array

--- Day_15/file.cpp
+++ Day_15/file.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void missingNumber(int arr[4]);
+void missingNumber(int arr[], int size);
+void addOne(vector<int> &v);
 
 int main()
 {
-    int array[4] = {1, 2, 4};
-    missingNumber(array);
+    int array[4] = {1, 2, 3, 5};
+    int size = sizeof(array) / sizeof(array[0]);
+    vector<int> vec = {9, 9, 9, 0};
+    missingNumber(array ,size);
+    // cout << "\nVector after before one:\n";
+    // for (auto x : vec)
+    // {
+    //     cout << x << " ";
+    // }
+    // addOne(vec);
+    // cout << "\nVector after adding one:\n";
+    // for (auto x : vec)
+    // {
+    //     cout << x << " ";
+    // }
+    cout << endl;
     return 0;
 }
 
-void missingNumber(int arr[4])
+void missingNumber(int arr[] , int size)
 {
     // Mehtod 1 - Sort array
     // for (int i = 1; i < 4; i++)
@@ -38,12 +54,29 @@ void missingNumber(int arr[4])
     //     cout << "No number is missing!" << endl;
 
     // Method 2
-    int exactSum = ((4 + 1) * (4 + 2)) / 2;
-    cout << "total sum of consective first 4 numbers!: " << exactSum << endl;
+    int exactSum = ((size + 1) * (size + 2)) / 2;
+    cout << "total sum of consective 5 numbers!: " << exactSum << endl;
     int sum = 0;
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < size; i++)
     {
         sum += arr[i];
     }
     cout << "Missing number is: " << (exactSum - sum) << endl;
+}
+
+void addOne(vector<int> &v)
+{
+    for (int i = v.size() - 1; i >= 0; i--)
+    {
+        if (v[i] == 9)
+        {
+            v[i] = 0;
+        }
+        else
+        {
+            v[i]++;
+            return;
+        }
+    }
+    v.insert(v.begin(), 1);
 }
--- Day_15/file.dart
+++ Day_15/file.dart
@@ -1,41 +1,52 @@
- void main() {
-  List<int> array = [1, 2, 4];
-  missingNumber(array);
+import 'dart:io';
 
+void main() {
+  List<int> array = [1, 2, 3, 5];
+  int size = array.length;
+
+  missingNumber(array, size);
+
+  List<int> vec = [9, 9, 9, 0];
+
+  print("\nVector before adding one:");
+  for (int x in vec) {
+    stdout.write("$x");
+  }
+
+  addOne(vec);
+
+  print("\nVector after adding one:");
+  for (int x in vec) {
+    stdout.write("$x ");
+  }
+  print("");
 }
 
-void missingNumber(List<int> array) {
-  // Mehtod 1 - Sort array
-  // for (int i = 1; i < 4; i++)
-  // {
-  //     int x = i;
-  //     while (x > 0 && arr[x] < arr[x - 1])
-  //     {
-  //         swap(arr[x], arr[x - 1]);
-  //         x--;
-  //     }
-  // }
-  // int missingNum;
-  // bool miss = false;
-  // for (int n = 0; n < 3; n++)
-  // {
-  //     if ((arr[n] + 1) != arr[n + 1])
-  //     {
-  //         miss = true;
-  //         missingNum = arr[n] + 1;
-  //     }
-  // }
-  // if (miss)
-  //      print("Missing number is: $missingNum");
-  // else
-  //     print("No number is missing");
+void missingNumber(List<int> arr, int size) {
+  // formula assumes numbers from 1 to (size + 1)
+  int exactSum = ((size + 1) * (size + 2)) ~/ 2;
+  print("\nTotal sum of consecutive ${size + 1} numbers: $exactSum");
 
-  // Method 2
-  int exactSum = ((4 + 1) * (4 + 2)) ~/ 2;
-  print("total sum of consective first 4 numbers!: $exactSum");
   int sum = 0;
-  for (int i = 0; i < 4; i++) {
-    sum += array[i];
+  for (int i = 0; i < size; i++) {
+    sum += arr[i];
   }
-  print("Missing number is: ${exactSum - sum} ");
+
+  int missing = exactSum - sum;
+  print("Missing number is: $missing");
+}
+
+void addOne(List<int> v) {
+  // move from last digit to first
+  for (int i = v.length - 1; i >= 0; i--) {
+    if (v[i] == 9) {
+      v[i] = 0; // carry continues
+    } else {
+      v[i] = v[i] + 1; // carry stops
+      return;
+    }
+  }
+
+  // if all digits were 9
+  v.insert(0, 1);
 }
