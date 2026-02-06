9734c9e0bbe2327ceba36702388b07f7769ef787
itxmal03 <itxmal03@users.noreply.github.com>
2026-02-06 23:28:41 +0500

    day 1 of DSA by Ma'am Shradha khapra

--- Day_19/file.cpp
+++ Day_19/file.cpp
@@ -1,8 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+void checkPrime(int n);
 
 int main()
 {
-    
+    checkPrime(5);
     return 0;
+}
+
+void checkPrime(int n)
+{
+    bool isPrime = true;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+        cout << "yes prime!" << endl;
+    else
+        cout << "not prime!" << endl;
 }
