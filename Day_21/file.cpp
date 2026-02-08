754db70f13fe76d288602a9d66c2b3b11be1251c
itxmal03 <itxmal03@users.noreply.github.com>
2026-02-08 23:03:16 +0500

    two's complement

---
+++ Day_21/file.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void twosComplement(int n);
+
+int main()
+{
+    twosComplement(101);
+    return 0;
+}
+
+void twosComplement(int n)
+{
+    string s = to_string(n);
+    // one's complement
+    if (s[0] == '-')
+    {
+        s[0] = 0;
+    }
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == 0)
+        {
+            s[i] = 1;
+        }
+        else
+        {
+            s[i] = 0;
+        }
+        
+    }
+    cout << s << endl;
+    cout << "oringinal number: " << n << " fliped number: " << s << endl;
+}
