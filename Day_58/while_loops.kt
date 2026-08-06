51ddb6fc23af86682adc54617e7f3b1532d51ebf
itxmal03 <aftabliaqat933@gmail.com>
2026-08-06 15:41:30 +0500

    while loops in kotlin

---
+++ Day_58/while_loops.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    var i = 0
+    while (i <= 5) {
+        println("Current number is $i")
+        i++
+    }
+
+    var j = 10
+
+    do {
+        println("Current number is $j")
+        j--
+    } while (j > 5)
+}
