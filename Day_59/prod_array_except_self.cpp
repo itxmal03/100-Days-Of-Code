beef848a6df4565e7d1b9e34b6c9aacaaae31125
itxmal03 <aftabliaqat933@gmail.com>
2026-08-08 21:43:00 +0500

    method 2 - 4 for product of array except self

--- Day_59/prod_array_except_self.cpp
+++ Day_59/prod_array_except_self.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int *method1(int *arr, int n)
@@ -18,6 +19,65 @@ int *method1(int *arr, int n)
     return ans;
 }
 
+int *method2(int *arr, int n)
+{
+    int *ans = new int[n];
+    for (int k = 0; k < n; k++)
+        ans[k] = 1; // FIX 1: Initialize all to 1
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j)
+            { // FIX 2: Compare indices, not values
+                ans[i] *= arr[j];
+            }
+        }
+    }
+    return ans;
+}
+
+vector<int> method3(const vector<int> &v)
+{
+    int n = v.size();
+    vector<int> prefix(n, 1), suffix(n, 1), ans(n, 1);
+
+    for (int i = 1; i < n; i++)
+    {
+        prefix[i] = prefix[i - 1] * v[i - 1];
+    }
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffix[i] = suffix[i + 1] * v[i + 1];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        ans[i] = prefix[i] * suffix[i];
+    }
+    return ans;
+}
+
+vector<int> method4(const vector<int> &v)
+{
+    int n = v.size();
+    vector<int> ans(n, 1);
+
+    for (int i = 1; i < n; i++)
+    {
+        ans[i] = ans[i - 1] * v[i - 1];
+    }
+
+    int suffix = 1;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffix *= v[i + 1];
+        ans[i] *= suffix;
+    }
+
+    return ans;
+}
+
 int main()
 {
     int array[5] = {1, 2, 3, 4, 5};
@@ -29,7 +89,7 @@ int main()
         cout << ansArray[i] << " ";
     }
 
-    delete ansArray;
+    delete[] ansArray;
 
     return 0;
 }
